3b104a2a78b3ecb47f9ac35fee25cfd4d670e33c
zx212 <sskhoda44@gmail.com>
2021-12-15 10:07:26 +0900

    プロジェクト ファイルを追加します。

---
+++ Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX29_09_sasakihodaka
+{
+    class Box
+    {
+        public static float GetVolume(float w, float h, float d)
+        {
+            return w * h * d;
+        }
+        public static float GetSurface(float w, float h, float d)
+        {
+            return (w * h + h * d + d * w) * 2;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EX29_09_sasakihodaka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"boxの体積={Box.GetVolume(1, 3.5f, 2)},Boxの表面積＝{Box.GetSurface(1, 3.5f, 2)}");
+        }
+    }
+}
